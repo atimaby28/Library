2b818ffc0f3212d2ab814842393c0ac5a54ef6d1
atimaby28 <atimaby28@gmail.com>
2024-03-03 19:08:00 +0900

    Update : borrow & return

---
+++ library-app/src/main/java/com/group/libraryapp/domain/user/loanhistory/UserLoanHistory.java
@@ -0,0 +1,2 @@
+package com.group.libraryapp.domain.user.loanhistory;public class UserLoanHistory {
+}
---
+++ library-app/src/main/java/com/group/libraryapp/domain/user/loanhistory/UserLoanHistoryRepository.java
@@ -0,0 +1,2 @@
+package com.group.libraryapp.domain.user.loanhistory;public interface UserLoanHistoryRepository {
+}
